5271dcf1aab8d2144e9f52784a213550123952c9
Tarax-ikuro <96568811+Tarax-ikuro@users.noreply.github.com>
2022-01-26 16:19:35 +0100

    Add files via upload

---
+++ Imagesboutons/script.js
@@ -0,0 +1,14 @@
+//MISE EN PLACE DE L'ACTION 
+//document.=>
+
+document.getElementById("lion").addEventListener("click",function () {
+    document.getElementById("animaux").src="images/lion.jpeg"
+});
+
+
+		document.getElementById("cobra").addEventListener("click", function() {
+			document.getElementById("animaux").src="images/cobra.jpeg"
+		});
+		document.getElementById("eagle").addEventListener("click", function() {
+			document.getElementById("animaux").src="images/eagle.jpg"
+		});
